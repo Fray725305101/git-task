50c2c8c06452d061c1b5fa18043e0af60e14ef2f
Андрей Пакудин <andrey.pakudin@yandex.ru>
2026-04-16 11:35:04 +0300

    fixed multiply and add sqrt

--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
-# Автор: ...
+# Автор: Андрей Пакудин
+import math
 
 def add(a, b):
     return a + b
 
-
 def subtract(a, b):
     return a - b
 
-
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
+    return a*b
 
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
